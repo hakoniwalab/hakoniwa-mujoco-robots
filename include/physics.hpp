1710b926033b144a24aa277d8512fa9323d074fd
Takashi Mori <kanetugu2015@gmail.com>
2025-03-28 06:11:25 +0900

    add headers

---
+++ include/physics.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "primitive_types.hpp"
+#include <memory>
+#include <string>
+
+namespace hako::robots::physics
+{
+
+    class IWorld
+    {
+    protected:
+        mjModel* model;
+        mjData* data;
+    public:
+        ~IWorld()
+        {
+            mj_deleteData(data);
+            mj_deleteModel(model);
+        }
+        virtual void loadModel(const std::string& model_file) = 0;
+        virtual void advanceTimeStep() = 0;
+        virtual std::shared_ptr<IRigidBody> getRigidBody(const std::string& model_name) = 0;
+    };
+
+    class IRigidBody
+    {
+    public:
+        virtual hako::robots::types::Position GetPosition() = 0;
+        virtual hako::robots::types::Euler GetEuler() = 0;
+        virtual hako::robots::types::Velocity GetVelocity() = 0;
+        virtual hako::robots::types::EulerRate GetEulerRate() = 0;
+        virtual hako::robots::types::BodyVelocity GetBodyVelocity() = 0;
+        virtual hako::robots::types::BodyAngularVelocity GetBodyAngularVelocity() = 0;
+
+        virtual void SetTorque(const std::string& joint_name, double torque) = 0;
+        virtual void SetForce(const std::string& body_name, const hako::robots::types::Vector3& force) = 0;
+
+    };
+}
---
+++ include/primitive_types.hpp
@@ -0,0 +1,147 @@
+#pragma once
+
+#include <mujoco/mujoco.h>
+#include <sstream>
+#include <iomanip>
+
+namespace hako::robots::types
+{
+    class Vector3
+    {
+    public:
+        double x;
+        double y;
+        double z;
+        double length() const {
+            return std::sqrt(x*x + y*y + z*z);
+        }
+        
+        Vector3 normalize() const {
+            double len = length();
+            return (len > 0) ? (*this / len) : Vector3();
+        }
+        std::string to_string() const {
+            std::ostringstream oss;
+            oss << std::fixed << std::setprecision(3);
+            oss << x << ", " << y << ", " << z;
+            return oss.str();
+        }
+        
+        //copy constructor
+        Vector3(const Vector3& v)
+        {
+            x = v.x;
+            y = v.y;
+            z = v.z;
+        }
+        //default constructor
+        Vector3()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+        // = operator
+        Vector3& operator=(const Vector3& v)
+        {
+            x = v.x;
+            y = v.y;
+            z = v.z;
+            return *this;
+        }
+        // + operator
+        Vector3 operator+(const Vector3& v) const
+        {
+            Vector3 result;
+            result.x = x + v.x;
+            result.y = y + v.y;
+            result.z = z + v.z;
+            return result;
+        }
+        // - operator
+        Vector3 operator-(const Vector3& v) const
+        {
+            Vector3 result;
+            result.x = x - v.x;
+            result.y = y - v.y;
+            result.z = z - v.z;
+            return result;
+        }
+        // * operator
+        Vector3 operator*(const double& scalar) const
+        {
+            Vector3 result;
+            result.x = x * scalar;
+            result.y = y * scalar;
+            result.z = z * scalar;
+            return result;
+        }
+        // / operator
+        Vector3 operator/(const double& scalar) const
+        {
+            Vector3 result;
+            result.x = x / scalar;
+            result.y = y / scalar;
+            result.z = z / scalar;
+            return result;
+        }
+
+    };
+
+    /*
+     * Position class
+     * Unit: Meters
+     * Frame: ROS standard frame
+     */
+    class Position: public Vector3
+    {
+    };
+    /*
+     * Attitude class
+     * Unit: Radians
+     * Frame: ROS standard frame
+     */
+    class Euler: public Vector3
+    {
+    };
+
+    /*
+     * Velocity class
+     * Unit: Meters per second
+     * Frame: ROS standard frame
+     * World frame
+     */
+    class Velocity: public Vector3
+    {
+    };
+
+    /*
+     * BodyVelocity class
+     * Unit: Meters per second
+     * Frame: ROS standard frame
+     * Body frame
+     */
+    class BodyVelocity: public Vector3
+    {
+    };
+
+    /*
+     * EulerRate class
+     * Unit: Radians per second
+     * Frame: ROS standard frame
+     * World frame
+     */
+    class EulerRate: public Vector3
+    {
+    };
+
+    /*
+     * BodyAngularVelocity class
+     * Unit: Radians per second
+     * Frame: ROS standard frame
+     * Body frame
+     */
+    class BodyAngularVelocity: public Vector3
+    {
+    };
+}
