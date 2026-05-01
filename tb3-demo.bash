d32da12459abe94a3f50adb448a5180e3f34ebf6
tmori <kanetugu2015@gmail.com>
2026-05-01 16:41:17 +0900

    add win launcher

--- tb3-demo.bash
+++ tb3-demo.bash
@@ -4,4 +4,12 @@
 # ACTIVATE_MODE="activate-only" bash tb3-demo.bash
 ACTIVATE_MODE=${ACTIVATE_MODE:-"immediate"}
 
- python3 -m hakoniwa_pdu.apps.launcher.hako_launcher --mode ${ACTIVATE_MODE} tb3-demo-launch.json
+if grep -qi microsoft /proc/version 2>/dev/null || grep -qi microsoft /proc/sys/kernel/osrelease 2>/dev/null; then
+    powershell.exe -Command "python -m hakoniwa_pdu.apps.launcher.hako_launcher --mode ${ACTIVATE_MODE} tb3-demo-launch-win.json"
+elif [ "$(uname -s)" = "Linux" ]; then
+    python3 -m hakoniwa_pdu.apps.launcher.hako_launcher --mode ${ACTIVATE_MODE} tb3-demo-launch.json
+elif [ "$(uname -s)" = "Darwin" ]; then
+    python3 -m hakoniwa_pdu.apps.launcher.hako_launcher --mode ${ACTIVATE_MODE} tb3-demo-launch.json
+else
+    echo "Unsupported OS"
+fi
